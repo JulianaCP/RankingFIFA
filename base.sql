928146e0d0fde4721d3589026772520353859611
Juliana Campos <JCP@hotmail.com>
2017-10-25 16:34:10 -0600

    actualizacion

--- base.sql
+++ base.sql
@@ -1,5 +1,7 @@
 --drop database proyecto_web_dos
 create database proyecto_web_dos
+use master
+use proyecto_web_dos;
 
 
 CREATE TABLE Confederacion(
@@ -7,7 +9,7 @@ CREATE TABLE Confederacion(
 	nombre VARCHAR(50)
 );
 
-use proyecto_web_dos;
+
 
 CREATE TABLE Equipo(
 	nombre VARCHAR(100) PRIMARY KEY,
@@ -38,7 +40,7 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Bélgica',1333,'img/imagenes_ranking/5_Belgica.png',1,6),
 		('Polonia',1323,'img/imagenes_ranking/6_Polonia.png',1,6),
 		('Francia',1226,'img/imagenes_ranking/7_Francia.png',1,6),
-		('España',1218,'img/imagenes_ranking/8_España.png',1,6),
+		('España',1218,'img/imagenes_ranking/8_Espana.png',1,6),
 		('Chile',1173,'img/imagenes_ranking/9_Chile.png',1,3),
 		('Peru',1160,'img/imagenes_ranking/10_Peru.png',1,3),
 		('Suiza',1134,'img/imagenes_ranking/11_Suiza.png',1,6),
@@ -63,7 +65,7 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Egipto',818,'img/imagenes_ranking/30_Egipto.png',1,1),
 		('Ucrania',818,'img/imagenes_ranking/31_Ucrania.png',1,6),
 		('Senegal',815,'img/imagenes_ranking/32_Senegal.png',1,1),
-		('Turquia',807,'img/imagenes_ranking/33_Turquía.png',1,6),
+		('Turquia',807,'img/imagenes_ranking/33_Turquia.png',1,6),
 		('Iran',784,'img/imagenes_ranking/34_Iran.png',1,5),
 		('RD del Congo',751,'img/imagenes_ranking/35_RD_Del_Congo.png',1,1),
 		('Paraguay',750,'img/imagenes_ranking/36_Paraguay.png',1,3),
@@ -71,16 +73,16 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Serbia',748,'img/imagenes_ranking/38_Serbia.png',1,6),
 		('Austria',745,'img/imagenes_ranking/39_Austria.png',1,6),
 		('Bosnia y Herzegovina',744,'img/imagenes_ranking/40_Bosnia_Herzegovina.png',1,6),
-		('Nigeria',721,'41_Nigeria.png',1,1),
-		('Camerún',718,'42_Camerún.png',1,1),
-		('Australia',714,'43_Australia.png',1,5),
-		('Japón',711,'44_Japon.png',1,5),
-		('Rumanía',705,'45_Rumania',1,6),
-		('República Checa',699,'46_República_Checa.png',1,6),
-		('Grecia',682,'47_Grecia.png',1,6),
-		('Marruecos',680,'48_Marruecos.png',1,1),
-		('Panamá',670,'49_Panama.png',1,2),
-		('Bolivia',664,'50_Bolivia.png',1,3),
+		('Nigeria',721,'img/imagenes_ranking/41_Nigeria.png',1,1),
+		('Camerún',718,'img/imagenes_ranking/42_Camerun.png',1,1),
+		('Australia',714,'img/imagenes_ranking/43_Australia.png',1,5),
+		('Japón',711,'img/imagenes_ranking/44_Japon.png',1,5),
+		('Rumanía',705,'img/imagenes_ranking/45_Rumania.png',1,6),
+		('República Checa',699,'img/imagenes_ranking/46_Republica_Checa.png',1,6),
+		('Grecia',682,'img/imagenes_ranking/47_Grecia.png',1,6),
+		('Marruecos',680,'img/imagenes_ranking/48_Marruecos.png',1,1),
+		('Panamá',670,'img/imagenes_ranking/49_Panama.png',1,2),
+		('Bolivia',664,'img/imagenes_ranking/50_Bolivia.png',1,3),
 		('Venezuela',663,'img/imagenes_ranking/51_Venezuela.png',1,3),
 		('Ghana',651,'img/imagenes_ranking/52_Ghana.png',1,1),
 		('Hungría',642,'img/imagenes_ranking/53_Hungria.png',1,6),
@@ -103,8 +105,8 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Uganda',503,'img/imagenes_ranking/70_Uganda.png',1,1),
 		('Albania',498,'img/imagenes_ranking/71_Albania.png',1,6),
 		('Emiratos Árabes Unidos',488,'img/imagenes_ranking/72_Emiratos_Arabes_Unidos.png',1,5),
-		('Mali',488,'img/imagenes_ranking/73_Mali.png',1,1),
-		('Finlandia',473,'img/imagenes_ranking/74_Finlandia.png',1,6),
+		('Finlandia',473,'img/imagenes_ranking/73_Finlandia.png',1,6),
+		('Mali',488,'img/imagenes_ranking/74_Mali.png',1,1),
 		('Sudáfrica',473,'img/imagenes_ranking/75_Sudafrica.png',1,1),
 		('Uzbekistán',470,'img/imagenes_ranking/76_Uzbekistan.png',1,5),
 		('Siria',450,'img/imagenes_ranking/77_Siria.png',1,5),
@@ -122,7 +124,7 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Curazao',402,'img/imagenes_ranking/89_Curazao.png',1,2),
 		('Armenia',392,'img/imagenes_ranking/90_Armenia.png',1,6),
 		('Bielorrusia',391,'img/imagenes_ranking/91_Bielorrusia.png',1,6),
-		('Gabón',384,'img/imagenes_ranking/92_Gabón.png',1,1),
+		('Gabón',384,'img/imagenes_ranking/92_Gabon.png',1,1),
 		('Luxemburgo',368,'img/imagenes_ranking/93_Luxemburgo.png',1,6),
 		('Congo',367,'img/imagenes_ranking/94_Congo.png',1,1),
 		('Islas Feroe',363,'img/imagenes_ranking/95_Islas_Feroe.png',1,6),
@@ -131,3 +133,6 @@ INSERT INTO Equipo(nombre,puntos,bandera,activado,idConfederacion)
 		('Qatar',1631,'img/imagenes_ranking/98_Qatar.png',1,5),
 		('Sierra Leona',1631,'img/imagenes_ranking/99_Sierra_Leona.png',1,1),
 		('Líbano',1631,'img/imagenes_ranking/100_Libano.png',1,5);
+
+
+select * from Equipo
